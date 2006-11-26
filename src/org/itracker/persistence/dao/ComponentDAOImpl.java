2a12026f5db24a369e0307df8d3d536c261b0bdb
johnnymac75 <johnnymac75@users.sf.net>
2006-11-26 09:49:12 +0000

    - Renamed findByPrimaryKey to findById - Renamed findByProjectId to findByProject - Improved implementation of findByProject

--- src/org/itracker/persistence/dao/ComponentDAOImpl.java
+++ src/org/itracker/persistence/dao/ComponentDAOImpl.java
@@ -6,27 +6,26 @@ import org.hibernate.criterion.Expression;
 import org.itracker.model.Component;
 import org.itracker.model.Project;
 
-public class ComponentDAOImpl extends BaseHibernateDAOImpl<Component> 
+public class ComponentDAOImpl extends BaseHibernateDAOImpl<Component>
         implements ComponentDAO {
-
-    public Component findByPrimaryKey(Integer componentId) {
+    
+    public Component findById(Integer componentId) {
         try {
             return (Component)getSession().get(Component.class, componentId);
         } catch (HibernateException e) {
             throw convertHibernateAccessException(e);
         }
     }
-
+    
     @SuppressWarnings("unchecked")
-    public List<Component> findByProjectId(Integer projectId) {
-            try {
-            Project project = (Project) getSession().load(Project.class,projectId);
+    public List<Component> findByProject(Integer projectId) {
+        try {
             return getSession().createCriteria(Component.class)
-                    .add(Expression.eq("project", project))
-                    .list();
+                .add(Expression.eq("project.id", projectId))
+                .list();
         } catch (HibernateException e) {
             throw convertHibernateAccessException(e);
         }
     }
-
+    
 }
