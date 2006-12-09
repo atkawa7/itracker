602318dc0b0f203a0d5944f8b0162c11a2f89716
MarkyGoldstein <MarkyGoldstein@users.sf.net>
2006-12-09 11:59:49 +0000

    Improved usage of Generics.

--- src/org/itracker/persistence/dao/CustomFieldValueDAO.java
+++ src/org/itracker/persistence/dao/CustomFieldValueDAO.java
@@ -1,6 +1,7 @@
 package org.itracker.persistence.dao;
 
 import java.util.List;
+
 import org.itracker.model.CustomFieldValue;
 
 /**
@@ -10,6 +11,6 @@ public interface CustomFieldValueDAO extends BaseDAO<CustomFieldValue> {
 
     public CustomFieldValue findByPrimaryKey(Integer customFieldId);
 
-    public List findAll();
+    public List<CustomFieldValue> findAll();
     
 }
