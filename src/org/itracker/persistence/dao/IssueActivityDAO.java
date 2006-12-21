ae53346ae6429ce85333a937de26212fbbbcafe2
johnnymac75 <johnnymac75@users.sf.net>
2006-12-21 09:00:38 +0000

    Reviewed all DAO queries to match the ones defined in iTracker 2 (see ejb-jar.xml)

--- src/org/itracker/persistence/dao/IssueActivityDAO.java
+++ src/org/itracker/persistence/dao/IssueActivityDAO.java
@@ -6,8 +6,32 @@ import org.itracker.model.IssueActivity;
 
 public interface IssueActivityDAO extends BaseDAO<IssueActivity> {
 
-    public List<IssueActivity> findByIssueId(Integer issueId);
+    /**
+     * Finds the activity with the given ID. 
+     * 
+     * @param activityId system ID of the activity to return
+     * @return activity instance of <tt>null</tt> if none exists
+     */
+    IssueActivity findById(Integer activityId);
+    
+    /**
+     * Finds all activities of a given Issue. 
+     * 
+     * TODO : rename to findByIssue()
+     * 
+     * @param issueId system ID of the issue of which to retrieve activities
+     * @return list of activities of the issue, in unspecified order
+     */
+    List<IssueActivity> findByIssueId(Integer issueId);
 
-    public List<IssueActivity> findByIssueIdAndNotification(Integer issueId, int i);
+    /**
+     * Finds all activities for an Issue with the given notification status. 
+     * 
+     * @param issueId system ID of the issue of which to retrieve activities
+     * @param notificationSent whether to return activities for which 
+     *        a notification has been sent or not
+     */
+    List<IssueActivity> findByIssueIdAndNotification(Integer issueId, 
+            boolean notificationSent);
 
 }
--- src/org/itracker/persistence/dao/IssueActivityDAOImpl.java
+++ src/org/itracker/persistence/dao/IssueActivityDAOImpl.java
@@ -1,28 +1,60 @@
 package org.itracker.persistence.dao;
 
 import java.util.List;
-
-import org.itracker.model.Issue;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.itracker.model.IssueActivity;
 
-
+/**
+ * 
+ */
 public class IssueActivityDAOImpl extends BaseHibernateDAOImpl<IssueActivity> 
         implements IssueActivityDAO {
 
-    private final IssueDAO issueDAO;
+    public IssueActivityDAOImpl() {
+    }
 
-    public IssueActivityDAOImpl(IssueDAO issueDAO) {
-        this.issueDAO = issueDAO;
+    public IssueActivity findById(Integer activityId) {
+        IssueActivity activity;
+        try {
+            activity = (IssueActivity)getSession().get(IssueActivity.class, 
+                    activityId);
+        } catch (HibernateException ex) {
+            throw convertHibernateAccessException(ex);
+        }
+        return activity;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<IssueActivity> findByIssueId(Integer issueId) {
+        List<IssueActivity> activities; 
+        
+        try {
+            Query query = getSession().getNamedQuery(
+                    "IssueActivitiesByIssueQuery");
+            query.setInteger("issueId", issueId);
+            activities = query.list();
+        } catch (HibernateException ex) {
+            throw convertHibernateAccessException(ex);
+        }
+        return activities;
     }
 
     @SuppressWarnings("unchecked")
-    public List<IssueActivity> findByIssueId(Integer issueId) {
-        Issue issue = issueDAO.findByPrimaryKey(issueId);
-        return issue.getActivities();
-    }
-
-    public List<IssueActivity> findByIssueIdAndNotification(Integer issueId, int i) {
-        throw new UnsupportedOperationException();
+    public List<IssueActivity> findByIssueIdAndNotification(Integer issueId, 
+            boolean notificationSent) {
+        List<IssueActivity> activities; 
+        
+        try {
+            Query query = getSession().getNamedQuery(
+                    "IssueActivitiesByIssueAndNotificationQuery");
+            query.setInteger("issueId", issueId);
+            query.setBoolean("notificationSent", notificationSent);
+            activities = query.list();
+        } catch (HibernateException ex) {
+            throw convertHibernateAccessException(ex);
+        }
+        return activities;
     }
 
 }
