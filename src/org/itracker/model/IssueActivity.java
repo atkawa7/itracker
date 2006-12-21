7df0c44b7cd7ed9eb5d25db1272f5a3f7f0b7c8f
johnnymac75 <johnnymac75@users.sf.net>
2006-12-21 10:33:17 +0000

    - javadoc - changed type of notificationSent from int to boolean - added methods equals, hashCode and toString

--- src/org/itracker/model/IssueActivity.java
+++ src/org/itracker/model/IssueActivity.java
@@ -18,49 +18,81 @@
 
 package org.itracker.model;
 
+import java.util.Date;
+
 /**
- * This is a POJO Business Domain Object. Hibernate Bean.
+ * This is a POJO Business Domain Object modelling an issue activity. 
+ * 
+ * <p>Hibernate Bean. </p>
+ * 
+ * <p>The natural key of an IssueActivity is issue + user + type + createDate. 
+ * </p>
+ * 
  * @author ready
- *
  */
 public class IssueActivity extends AbstractBean {
-
-    private User user;
-    private int type;
-    private String description;
-    private int notificationSent;
+    
+    /** Issue to which this activity is related. */
     private Issue issue;
-
-    public int getType() {
-        return type;
+    
+    /** User who generated this activity. */
+    private User user;
+    
+    /** Type of activity. */
+    private int type;
+    
+    /** Optional activity description. */
+    private String description;
+    
+    /** 
+     * Whether a notification has been sent for this activity.
+     */
+    private boolean notificationSent;
+    
+    /**
+     * Default constructor (required by Hibernate). 
+     * 
+     * <p>PENDING: should be <code>private</code> so that it can only be used
+     * by Hibernate, to ensure that <code>issue</code>, <code>user</code> 
+     * and <code>type</code>, which form an instance's identity, 
+     * are always initialized. </p>
+     */
+    public IssueActivity() {
     }
-
-    public void setType(int value) {
-        type = value;
+    
+    /**
+     * Creates a new instance with a <code>notificationSent</code> flag set 
+     * to <tt>false</tt> and a creation and last modified time stamp 
+     * set to the current time. 
+     * 
+     * @param issue
+     * @param user
+     * @param type 
+     * @param description 
+     */
+    public IssueActivity(Issue issue, User user, int type, String description) {
+        this(issue, user, type, description, false);
     }
-
-    public String getDescription() {
-        return description;
+    
+    public IssueActivity(Issue issue, User user, int type, String description, 
+            boolean notificationSent) {
+        setIssue(issue);
+        setUser(user);
+        setType(type);
+        setDescription(description);
+        setNotificationSent(notificationSent);
+        setCreateDate(new Date());
     }
-
-    public void setDescription(String value) {
-        this.description = value;
-    }
-
-    public int getNotificationSent() {
-        return notificationSent;
-    }
-
-    public void setNotificationSent(int value) {
-        this.notificationSent = value;
-    }
-
+    
     public Issue getIssue() {
-        return (issue);
+        return issue;
     }
 
-    public void setIssue(Issue value) {
-        this.issue = value;
+    public void setIssue(Issue issue) {
+        if (issue == null) {
+            throw new IllegalArgumentException("null issue");
+        }
+        this.issue = issue;
     }
 
     public User getUser() {
@@ -68,7 +100,64 @@ public class IssueActivity extends AbstractBean {
     }
 
     public void setUser(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException("null user");
+        }
         this.user = user;
     }
+    
+    public int getType() {
+        return type;
+    }
 
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getNotificationSent() {
+        return notificationSent;
+    }
+
+    public void setNotificationSent(boolean sent) {
+        this.notificationSent = sent;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        
+        if (obj instanceof IssueActivity) {
+            final IssueActivity other = (IssueActivity)obj;
+            
+            return this.issue.equals(other.issue)
+                && this.user.equals(other.user)
+                && (this.type == other.type)
+                && this.createDate.equals(other.createDate);
+        }
+        return false;
+    }
+    
+    public int hashCode() {
+        return this.issue.hashCode()
+            + this.user.hashCode()
+            + this.type
+            + this.createDate.hashCode();
+    }
+    
+    public String toString() {
+        return "[issue=" + this.issue 
+                + ",user=" + this.user 
+                + ",type=" + this.type 
+                + ",createDate=" + this.createDate + "]";
+    }
+    
 }
