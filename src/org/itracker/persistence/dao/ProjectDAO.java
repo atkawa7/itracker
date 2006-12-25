94d57d825227ca50ed26005c55a617fffbfaa53c
johnnymac75 <johnnymac75@users.sf.net>
2006-12-25 13:17:13 +0000

    - Added findByStatus - Javadoc - removed  "public" modified: it is implicit for all interface methods

--- src/org/itracker/persistence/dao/ProjectDAO.java
+++ src/org/itracker/persistence/dao/ProjectDAO.java
@@ -9,10 +9,34 @@ import org.itracker.model.Project;
  */
 public interface ProjectDAO extends BaseDAO<Project> {
     
-    public Project findByPrimaryKey(Integer projectId);
+    /**
+     * Finds a project by ID. 
+     * 
+     * @param projectId sytem ID
+     * @return project instance or <tt>null</tt>
+     */
+    Project findByPrimaryKey(Integer projectId);
 
-    public List<Project> findAll();
+    /**
+     * Finds all projects. 
+     * 
+     * @return list of all existing projects, in unspecified order
+     */
+    List<Project> findAll();
 
-    public List<Project> findAllAvailable();
+    /**
+     * Finds all projects with a given status. 
+     * 
+     * @param status project status
+     * @return list of projects in unspecified order
+     */
+    List<Project> findByStatus(int status);
+    
+    /**
+     * Finds all projects that are active or viewable. 
+     * 
+     * @return list of projects in unspecified order
+     */
+    List<Project> findAllAvailable();
     
 }
