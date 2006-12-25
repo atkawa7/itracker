269e5f2b92bcc4aaf19523c14553615e2092b369
johnnymac75 <johnnymac75@users.sf.net>
2006-12-25 13:24:44 +0000

    removed  "public" modifier: it is implicit for all interface methods

--- src/org/itracker/services/ConfigurationService.java
+++ src/org/itracker/services/ConfigurationService.java
@@ -18,9 +18,9 @@
 
 package org.itracker.services;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Properties;
 
 import org.itracker.model.Configuration;
@@ -35,19 +35,19 @@ import org.itracker.model.ProjectScript;
 
 public interface ConfigurationService {
     
-    public Properties getProperties();
+    Properties getProperties();
 
-    public String getProperty(String name);
+    String getProperty(String name);
     
-    public String getProperty(String name, String defaultValue);
+    String getProperty(String name, String defaultValue);
     
-    public boolean getBooleanProperty(String name, boolean defaultValue);
+    boolean getBooleanProperty(String name, boolean defaultValue);
     
-    public int getIntegerProperty(String name, int defaultValue);
+    int getIntegerProperty(String name, int defaultValue);
     
-    public long getLongProperty(String name, long defaultValue);
+    long getLongProperty(String name, long defaultValue);
 
-    public Configuration getConfigurationItem(Integer id);
+    Configuration getConfigurationItem(Integer id);
     
     /**
       * Returns all the configuration items of a particular type.  The name values
@@ -55,7 +55,7 @@ public interface ConfigurationService {
       * @param type the type of configuration items to retrieve
       * @return an array of ConfigurationModels
       */
-    public List<Configuration> getConfigurationItemsByType(int type);
+    List<Configuration> getConfigurationItemsByType(int type);
     
     /**
       * Returns all the configuration items of a particular type.  In addition, all
@@ -65,14 +65,14 @@ public interface ConfigurationService {
       * @param locale the locale to use when setting the configuration items name values
       * @return an array of ConfigurationModels
       */   
-    public List<Configuration> getConfigurationItemsByType(int type, Locale locale);
+    List<Configuration> getConfigurationItemsByType(int type, Locale locale);
 
     /**
       * This method will create a new Configuration for persistance in the database.
       * @param model the model to create the bean from
       * @returns and updated model with the information from the newly created bean
       */
-    public Configuration createConfigurationItem(Configuration configuration);
+    Configuration createConfigurationItem(Configuration configuration);
  
     /**
       * This method updates a configuration item in the database.  It does not include any updates
@@ -80,26 +80,26 @@ public interface ConfigurationService {
       * @param model a Configuration of the item to update
       * @return a Configuration with the updated item
       */
-    public Configuration updateConfigurationItem(Configuration configuration);
+    Configuration updateConfigurationItem(Configuration configuration);
     
-    public List<Configuration> updateConfigurationItems(List<Configuration> configurations, int type);
+    List<Configuration> updateConfigurationItems(List<Configuration> configurations, int type);
 
-    public boolean configurationItemExists(Configuration configuration);
+    boolean configurationItemExists(Configuration configuration);
     
-    public boolean configurationItemUpToDate(Configuration configuration);
+    boolean configurationItemUpToDate(Configuration configuration);
 
     /**
       * This method will remove the configuration item with the supplied id.
       * @param id the id of the configuration information to remove
       */
-    public void removeConfigurationItem(Integer id);
+    void removeConfigurationItem(Integer id);
  
     /**
       * This method will remove all configuration items that match the supplied type.  This
       * will remove all items of that type such as all system status values.
       * @param type the type of configuration information to remove
       */
-    public void removeConfigurationItems(int type);
+    void removeConfigurationItems(int type);
  
     /**
       * This method will remove all configuration items that match the supplied models
@@ -107,105 +107,116 @@ public interface ConfigurationService {
       * It is normally called prior to a create to remove any older copies of this item.
       * @param model the model to determine the type and value from
       */
-    public void removeConfigurationItems(Configuration configuration);
+    void removeConfigurationItems(Configuration configuration);
 
     /**
       * This method will reset any caches in the system of configuration items for all configuration
       * item types.
       */
-    public void resetConfigurationCache();
+    void resetConfigurationCache();
  
     /**
       * This method will reset any caches in the system of configuration items for the specified
       * configuration item type.
       * @param type the type of configuration item to reset in any caches
       */
-    public void resetConfigurationCache(int type);
+    void resetConfigurationCache(int type);
 
     /**
       * This method will return the requested project script.
       * @param id the id of the requested script
       * @return a ProjectScript with the requested script, or null if not found
       */
-    public ProjectScript getProjectScript(Integer id);
+    ProjectScript getProjectScript(Integer id);
+    
     /**
       * This method will return all defined project scripts.
       * @return a ProjectScript array with all defined scripts
       */
-    public List<ProjectScript> getProjectScripts();
+    List<ProjectScript> getProjectScripts();
+    
     /**
       * This method will create a new project script for persistance in the database.
       * @param model the model to create the script from
       * @returns and updated model with the information from the newly created script
       */
-    public ProjectScript createProjectScript(ProjectScript projectScript);
+    ProjectScript createProjectScript(ProjectScript projectScript);
+    
     /**
       * This method updates a project script in the database.
       * @param model a ProjectScript of the item to update
       * @return a ProjectScript with the updated item
       */
-    public ProjectScript updateProjectScript(ProjectScript projectScript);
+    ProjectScript updateProjectScript(ProjectScript projectScript);
+    
     /**
       * This method removes a project script in the database.
       * @param id  The id of the project script
       */
-    public void removeProjectScript(Integer id);
+    void removeProjectScript(Integer id);
 
     /**
       * This method will return the requested workflow script.
       * @param id the id of the requested script
       * @return a WorkflowScript with the requested script, or null if not found
       */
-    public WorkflowScript getWorkflowScript(Integer id);
+    WorkflowScript getWorkflowScript(Integer id);
+    
     /**
       * This method will return all defined workflow scripts.
       * @return a WorkflowScript array with all defined scripts
       */
-    public List<WorkflowScript> getWorkflowScripts();
+    List<WorkflowScript> getWorkflowScripts();
+    
     /**
       * This method will create a new workflow script for persistance in the database.
       * @param model the model to create the script from
       * @returns and updated model with the information from the newly created script
       */
-    public WorkflowScript createWorkflowScript(WorkflowScript workflowScript);
+    WorkflowScript createWorkflowScript(WorkflowScript workflowScript);
+    
     /**
       * This method updates a workflow script in the database.
       * @param model a WorkflowScript of the item to update
       * @return a WorkflowScript with the updated item
       */
-    public WorkflowScript updateWorkflowScript(WorkflowScript workflowScript);
+    WorkflowScript updateWorkflowScript(WorkflowScript workflowScript);
+    
     /**
       * This method removes a workflow script in the database.
       * @param id  The id of the workflow script
       */
-    public void removeWorkflowScript(Integer id);
+    void removeWorkflowScript(Integer id);
 
     /**
       * This method will return the requested custom field.
       * @param id the id of the requested field
       * @return a CustomField with the requested field, or null if not found
       */
-    
-    public CustomField getCustomField(Integer id);
+    CustomField getCustomField(Integer id);
+
     /**
       * This method will return all the custom fields defined in the system.
       * @return an array of CustomFieldModels
       */
-    public List<CustomField> getCustomFields();
+    List<CustomField> getCustomFields();
+    
     /**
       * This method will return all the custom fields defined in the system.  It will
       * also initialize all of the field labels using the supplied locale.
       * @param locale the locale to use to initialize the labels
       * @return an array of CustomFieldModels
       */
-    public List<CustomField> getCustomFields(Locale locale);
+    List<CustomField> getCustomFields(Locale locale);
+    
     /**
       * This method will create a new CustomField for persistance in the database.  If the
       * field includes a set of option values, those will be created also.
       * @param model the model to create the field from
       * @returns and updated model with the information from the newly created field
       */
-    public CustomField createCustomField(CustomField customField);
+    CustomField createCustomField(CustomField customField);
+    
     /**
       * This method updates a custom field in the database.  It does not include any updates
       * to language items that would be used to display the localized label for the field.
@@ -213,31 +224,36 @@ public interface ConfigurationService {
       * @param model a CustomField of the item to update
       * @return a CustomField with the updated item
       */
-    public CustomField updateCustomField(CustomField customField);
+    CustomField updateCustomField(CustomField customField);
+    
     /**
       * Removes a single custom field from the database.
       * @param customFieldValueId the id of the custom field to remove
       */
-    public void removeCustomField(Integer customFieldId);
+    void removeCustomField(Integer customFieldId);
+    
     /**
       * This method will return the requested custom field value.
       * @param id the id of the requested field value
       * @return a CustomField with the requested field value, or null if not found
       */
-    public CustomFieldValue getCustomFieldValue(Integer id);
+    CustomFieldValue getCustomFieldValue(Integer id);
+    
     /**
       * This method will create a new CustomFieldValue for persistance in the database.
       * @param model the model to create the field from
       * @returns and updated model with the information from the newly created field
       */
-    public CustomFieldValue createCustomFieldValue(CustomFieldValue customFieldValue);
+    CustomFieldValue createCustomFieldValue(CustomFieldValue customFieldValue);
+    
     /**
       * This method updates a custom field value in the database.  It does not include any updates
       * to language items that would be used to display the localized label for the field value.
       * @param model a CustomFieldValue of the item to update
       * @return a CustomFieldValue with the updated item
       */
-    public CustomFieldValue updateCustomFieldValue(CustomFieldValue customFieldValue);
+    CustomFieldValue updateCustomFieldValue(CustomFieldValue customFieldValue);
+    
     /**
       * This method updates a set of custom field values in the database.  If the array of values
       * is null or zero length, it will remove all existing values from the custom field.  Otherwise
@@ -248,74 +264,79 @@ public interface ConfigurationService {
       * @param models an array of CustomFieldValueModels to update
       * @return a array of CustomFieldValueModels with the updated items
       */
- 
-    public List<CustomFieldValue> updateCustomFieldValues(Integer customFieldId, List<CustomFieldValue> customFieldValues);
+    List<CustomFieldValue> updateCustomFieldValues(Integer customFieldId, 
+            List<CustomFieldValue> customFieldValues);
+    
     /**
       * Removes a single custom field value from the database.
       * @param customFieldValueId the id of the custom field value to remove
       */
-    public void removeCustomFieldValue(Integer customFieldValueId);
+    void removeCustomFieldValue(Integer customFieldValueId);
+    
     /**
       * Removes all custom field values from the database for a single custom field.
       * @param customFieldId the id of the custom field to remove the values for
       */
-    public void removeCustomFieldValues(Integer customFieldId);
-
+    void removeCustomFieldValues(Integer customFieldId);
+    
     /**
       * This method will return the translation for a particular key in a locale.
       * @param key the key to look up
       * @param locale the localue to translate the key for
       * @return a Language with the translation
       */
-    public Language getLanguageItemByKey(String key, Locale locale);
+    Language getLanguageItemByKey(String key, Locale locale);
+    
     /**
       * This method will return all the translations for a particular key.
       * @param key the key to look up
       * @return an array of LanguageModels with the translations for the key
       */
-    public List<Language> getLanguageItemsByKey(String key);
+    List<Language> getLanguageItemsByKey(String key);
+    
     /**
       * Updates a translations for a particular key and locale.
       * @param model A Language for the key to update
       * @return a Language with the updated translation
       */
-    public Language updateLanguageItem(Language language);
+    Language updateLanguageItem(Language language);
+    
     /**
       * This method will remove all language items with the supplied key regardless
       * of locale.
       * @param key the key to remove
       */
-    public void removeLanguageKey(String key);
+    void removeLanguageKey(String key);
     
-    public void removeLanguageItem(Language language);
+    void removeLanguageItem(Language language);
 
     /**
       * This method will return the current configuration of the system.
       * @return a SystemConfiguration with the current configuration of the system
       */
-    public SystemConfiguration getSystemConfiguration(Locale locale);
+    SystemConfiguration getSystemConfiguration(Locale locale);
 
     /**
       * Returns all of the keys currently defined in the base locale sorted and grouped in a
       * logical manner.
       */
-    public String[] getSortedKeys();
+    String[] getSortedKeys();
 
-    public HashMap<String,String> getDefinedKeys(String locale);
+    Map<String,String> getDefinedKeys(String locale);
     
-    public List<NameValuePair> getDefinedKeysAsArray(String locale);
+    List<NameValuePair> getDefinedKeysAsArray(String locale);
     
-    public int getNumberDefinedKeys(String locale);
+    int getNumberDefinedKeys(String locale);
     
-    public HashMap<String,List<String>> getAvailableLanguages();
+    Map<String,List<String>> getAvailableLanguages();
     
-    public int getNumberAvailableLanguages();
+    int getNumberAvailableLanguages();
     
-    public List<Language> getLanguage(Locale locale);
+    List<Language> getLanguage(Locale locale);
     
-    public void updateLanguage(Locale locale, List<Language> languages);
+    void updateLanguage(Locale locale, List<Language> languages);
     
-    public void updateLanguage(Locale locale, List<Language> languages, Configuration config);
+    void updateLanguage(Locale locale, List<Language> languages, Configuration config);
 
     /**
       * This method will load the specified locale.  It will look for the appropriate properties file,
@@ -324,13 +345,13 @@ public interface ConfigurationService {
       * @param forceReload if true, it will reload the languages from the property file even if it is listed
       *                    as being up to date
       */
-    public boolean initializeLocale(String locale, boolean forceReload);
+    boolean initializeLocale(String locale, boolean forceReload);
 
     /**
       * This method will load the some default system configuration data into the database.  The values
       * it loads are determined from the base ITracker.properties file so the language intiialization
       * <b>must</b> be performed before this method is called.
       */
-    public void initializeConfiguration();
+    void initializeConfiguration();
     
 }
