e3e588115e84cff2968acdfacf35ed252a414438
johnnymac75 <johnnymac75@users.sf.net>
2006-12-25 13:16:13 +0000

    - Javadoc - removed  "public" modified: it is implicit for all interface methods

--- src/org/itracker/persistence/dao/PermissionDAO.java
+++ src/org/itracker/persistence/dao/PermissionDAO.java
@@ -9,7 +9,22 @@ import org.itracker.model.Permission;
  */
 public interface PermissionDAO extends BaseDAO<Permission> {
 
-    public List<Permission> findByProjectIdAndPermission(Integer projectId, int permissionType);
+    /**
+     * Finds all Permissions granted to a user. 
+     * 
+     * @param userId 
+     * @return list of permissions granted to the given user, in unspecified order
+     */
+    List<Permission> findByUserId(Integer userId);
+    
+    /**
+     * Finds all Permissions of a given type granted on a project. 
+     * 
+     * @param projectId only permissions on this project will be returned
+     * @param permissionType type of permissions to return 
+     * @return list of permissions, in unspecified order
+     */
+    List<Permission> findByProjectIdAndPermission(Integer projectId, 
+            int permissionType);
 
-    public List<Permission> findByUserId(Integer id);
 }
