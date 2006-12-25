1e2265fa8bc2fdbbab1090437fcb6861e644fd0f
johnnymac75 <johnnymac75@users.sf.net>
2006-12-25 13:16:36 +0000

    - Converted all queries to named HQL queries - Reviewed all queries with iTracker 2 ejb-jar.xml

--- src/org/itracker/persistence/dao/PermissionDAOImpl.java
+++ src/org/itracker/persistence/dao/PermissionDAOImpl.java
@@ -1,38 +1,43 @@
 package org.itracker.persistence.dao;
 
 import java.util.List;
-
-import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
-import org.hibernate.criterion.Expression;
+import org.hibernate.Query;
 import org.itracker.model.Permission;
 
 public class PermissionDAOImpl extends BaseHibernateDAOImpl<Permission> 
         implements PermissionDAO {
     
     @SuppressWarnings("unchecked")
-    public List<Permission> findByProjectIdAndPermission(Integer projectId, int permissionType) {
-        Criteria criteria = getSession().createCriteria(Permission.class)
-            .add(Expression.eq("permissionType", permissionType))
-            .add(Expression.eq("project.id", projectId));
-        
-        try {
-            return criteria.list();
-        } catch (HibernateException e) {
-            throw convertHibernateAccessException(e);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
     public List<Permission> findByUserId(Integer userId) {
-        Criteria criteria = getSession().createCriteria(Permission.class)
-            .add(Expression.eq("user.id", userId));
+        List<Permission> permissions;
         
         try {
-            return criteria.list();
+            Query query = getSession().getNamedQuery(
+                    "PermissionsByUserQuery");
+            query.setInteger("userId", userId);
+            permissions = query.list();
         } catch (HibernateException ex) {
             throw convertHibernateAccessException(ex);
         }
+        return permissions;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Permission> findByProjectIdAndPermission(Integer projectId, 
+            int permissionType) {
+        List<Permission> permissions;
+        
+        try {
+            Query query = getSession().getNamedQuery(
+                    "PermissionsByProjectAndTypeQuery");
+            query.setInteger("projectId", projectId);
+            query.setInteger("permissionType", permissionType);
+            permissions = query.list();
+        } catch (HibernateException ex) {
+            throw convertHibernateAccessException(ex);
+        }
+        return permissions;
     }
 
 }
