ac056444a8afeac5912e55b030603b76d55ac70a
johnnymac75 <johnnymac75@users.sf.net>
2006-12-25 13:14:18 +0000

    - Added findById - Javadoc - removed  "public" modified: it is implicit for all interface methods

--- src/org/itracker/persistence/dao/LanguageDAO.java
+++ src/org/itracker/persistence/dao/LanguageDAO.java
@@ -1,16 +1,35 @@
 package org.itracker.persistence.dao;
 
 import java.util.List;
-
 import org.itracker.model.Language;
-
 
 public interface LanguageDAO extends BaseDAO<Language> {
 
-    public Language findByKeyAndLocale(String key, String base_locale);
+    Language findById(Integer id);
+    
+    /**
+     * Finds all language items with the given key and locale. 
+     * 
+     * @param key resource key
+     * @param locale 
+     * @return language items for the given locale
+     */
+    Language findByKeyAndLocale(String key, String locale);
+    
+    /**
+     * Finds all language items with a particular key.
+     * 
+     * @param key resource key
+     * @return language items with the given key for all available locales
+     */
+    List<Language> findByKey(String key);
 
-    public List<Language> findByKey(String key);
-
-    public List<Language> findByLocale(String base_locale);
+    /**
+     * Finds all language items with a given locale. 
+     * 
+     * @param locale 
+     * @return language items for the given locale
+     */
+    List<Language> findByLocale(String locale);
     
 }
