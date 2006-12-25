d90d5432203c6ec87c4f1c055be315729c2ed00f
johnnymac75 <johnnymac75@users.sf.net>
2006-12-25 13:45:51 +0000

    Use IssueForm instead of AbstractForm with PropertyUtils

--- src/org/itracker/web/actions/project/CreateIssueAction.java
+++ src/org/itracker/web/actions/project/CreateIssueAction.java
@@ -21,12 +21,12 @@ package org.itracker.web.actions.project;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.ResourceBundle;
 import java.util.Set;
 
 import javax.servlet.ServletException;
@@ -43,6 +43,7 @@ import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.ActionMessages;
 import org.apache.struts.upload.FormFile;
 import org.apache.struts.validator.ValidatorForm;
+import org.itracker.core.resources.ITrackerResources;
 import org.itracker.model.CustomField;
 import org.itracker.model.Issue;
 import org.itracker.model.IssueAttachment;
@@ -60,6 +61,7 @@ import org.itracker.services.util.ProjectUtilities;
 import org.itracker.services.util.UserUtilities;
 import org.itracker.services.util.WorkflowUtilities;
 import org.itracker.web.actions.base.ItrackerBaseAction;
+import org.itracker.web.forms.IssueForm;
 import org.itracker.web.util.AttachmentUtilities;
 import org.itracker.web.util.Constants;
 
@@ -73,10 +75,11 @@ public class CreateIssueAction extends ItrackerBaseAction {
         ActionErrors errors = new ActionErrors();
         super.executeAlways(mapping,form,request,response);
         
-        if(! isLoggedIn(request, response)) {
+        if (!isLoggedIn(request, response)) {
             return mapping.findForward("login");
         }
-        if(! isTokenValid(request)) {
+        
+        if (! isTokenValid(request)) {
             logger.info("Invalid request token while creating issue.");
             ProjectService projectService = getITrackerServices().getProjectService();
             request.setAttribute("projects",projectService.getAllProjects());
@@ -89,35 +92,41 @@ public class CreateIssueAction extends ItrackerBaseAction {
             IssueService issueService = getITrackerServices().getIssueService();
             ProjectService projectService = getITrackerServices().getProjectService();
             
-            
-            
             HttpSession session = request.getSession(true);
             User currUser = (User) session.getAttribute(Constants.USER_KEY);
             Map<Integer, Set<PermissionType>> userPermissionsMap = getUserPermissions(session);
             Locale currLocale = (Locale) session.getAttribute(Constants.LOCALE_KEY);
             Integer currUserId = currUser.getId();
             
+            IssueForm issueForm = (IssueForm)form;
+            
             Project project = null;
-            Integer projectId = (Integer) PropertyUtils.getSimpleProperty(form, "projectId");
-            if(projectId == null) {
-                errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("itracker.web.error.invalidproject"));
+            Integer projectId = issueForm.getProjectId();
+            
+            if (projectId == null) {
+                errors.add(ActionMessages.GLOBAL_MESSAGE,
+                        new ActionMessage("itracker.web.error.invalidproject"));
             } else {
                 project = projectService.getProject(projectId);
             }
             
-            if(errors.isEmpty() && project == null) {
-                errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("itracker.web.error.invalidproject"));
+            if (errors.isEmpty() && project == null) {
+                errors.add(ActionMessages.GLOBAL_MESSAGE,
+                        new ActionMessage("itracker.web.error.invalidproject"));
             } else if(errors.isEmpty() && project.getStatus() != ProjectUtilities.STATUS_ACTIVE) {
-                errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("itracker.web.error.projectlocked"));
-            } else if(! UserUtilities.hasPermission(userPermissionsMap, projectId, UserUtilities.PERMISSION_CREATE)) {
+                errors.add(ActionMessages.GLOBAL_MESSAGE,
+                        new ActionMessage("itracker.web.error.projectlocked"));
+            } else if(! UserUtilities.hasPermission(userPermissionsMap,
+                    projectId, UserUtilities.PERMISSION_CREATE)) {
                 return mapping.findForward("unauthorized");
             } else {
                 List<ProjectScript> scripts = project.getScripts();
-                WorkflowUtilities.ProcessFieldScripts(scripts, WorkflowUtilities.EVENT_FIELD_ONPRESUBMIT, null, errors, (ValidatorForm) form);
+                WorkflowUtilities.processFieldScripts(scripts,
+                        WorkflowUtilities.EVENT_FIELD_ONPRESUBMIT, null, errors, (ValidatorForm) form);
                 
                 Issue issue = new Issue();
-                issue.setDescription((String) PropertyUtils.getSimpleProperty(form, "description"));
-                issue.setSeverity(((Integer) PropertyUtils.getSimpleProperty(form, "severity")).intValue());
+                issue.setDescription((String) issueForm.getDescription());
+                issue.setSeverity(issueForm.getSeverity());
                 issue.setStatus(IssueUtilities.STATUS_NEW);
                 
                 Integer creator = currUserId;
@@ -126,30 +135,38 @@ public class CreateIssueAction extends ItrackerBaseAction {
                     creator = (Integer) PropertyUtils.getSimpleProperty(form, "creatorId");
                     logger.debug("New issue creator set to " + creator + ".  Issue created by " + currUserId);
                 }*/
-                issue = issueService.createIssue(issue, projectId, (creator == null ? currUserId : creator), currUserId);
+                issue = issueService.createIssue(issue, projectId,
+                        (creator == null ? currUserId : creator), currUserId);
                 
-                if(issue != null) {
-                    Integer newOwner = (Integer) PropertyUtils.getSimpleProperty(form, "ownerId");
-                    if(newOwner != null && newOwner.intValue() >= 0) {
-                        if(UserUtilities.hasPermission(userPermissionsMap, UserUtilities.PERMISSION_ASSIGN_OTHERS) ||
-                                (UserUtilities.hasPermission(userPermissionsMap, UserUtilities.PERMISSION_ASSIGN_SELF) &&
+                if (issue != null) {
+                    Integer newOwner = issueForm.getOwnerId();
+                    
+                    if (newOwner != null && newOwner.intValue() >= 0) {
+                        if (UserUtilities.hasPermission(userPermissionsMap,
+                                UserUtilities.PERMISSION_ASSIGN_OTHERS) ||
+                                (UserUtilities.hasPermission(userPermissionsMap,
+                                UserUtilities.PERMISSION_ASSIGN_SELF) &&
                                 currUserId.equals(newOwner))) {
                             issueService.assignIssue(issue.getId(), newOwner, currUserId);
                         }
                     }
                     
                     List<IssueField> issueFields = new ArrayList<IssueField>();
-                    HashMap customFields = (HashMap) PropertyUtils.getSimpleProperty(form, "customFields");
-                    if(customFields != null && customFields.size() > 0) {
+                    Map customFields = issueForm.getCustomFields();
+                    
+                    if (customFields != null && customFields.size() > 0) {
                         List<IssueField> issueFieldsVector = new ArrayList<IssueField>();
+                        ResourceBundle bundle = ITrackerResources.getBundle(currLocale);
+                        
                         for(Iterator iter = customFields.keySet().iterator(); iter.hasNext(); ) {
                             try {
                                 Integer fieldId = new Integer((String) iter.next());
                                 CustomField field = IssueUtilities.getCustomField(fieldId);
                                 String fieldValue = (String) PropertyUtils.getMappedProperty(form, "customFields(" + fieldId + ")");
-                                if(fieldValue != null && ! fieldValue.equals("")) {
-                                    IssueField issueField = new IssueField(field, issue);
-                                    issueField.setValue(fieldValue, currLocale);
+                                
+                                if (fieldValue != null && ! fieldValue.equals("")) {
+                                    IssueField issueField = new IssueField(issue, field);
+                                    issueField.setValue(fieldValue, currLocale, bundle);
                                     issueFieldsVector.add(issueField);
                                 }
                             } catch(Exception e) {
@@ -161,22 +178,25 @@ public class CreateIssueAction extends ItrackerBaseAction {
                     issueService.setIssueFields(issue.getId(), issueFields);
                     
                     
-                    IssueHistory issueHistory = new IssueHistory((String) PropertyUtils.getSimpleProperty(form, "history"),
-                            IssueUtilities.HISTORY_STATUS_AVAILABLE, issue, currUser);
+                    IssueHistory issueHistory = new IssueHistory(issue, currUser, 
+                            issueForm.getHistory(),
+                            IssueUtilities.HISTORY_STATUS_AVAILABLE);
                     issueHistory.setCreateDate(new Date());
                     issueService.addIssueHistory(issueHistory);
                     
                     HashSet<Integer> components = new HashSet<Integer>();
-                    Integer[] componentIds = (Integer[]) PropertyUtils.getSimpleProperty(form, "components");
-                    if(componentIds != null) {
-                        for(int i = 0; i < componentIds.length; i++) {
+                    Integer[] componentIds = issueForm.getComponents();
+                    
+                    if (componentIds != null) {
+                        for (int i = 0; i < componentIds.length; i++) {
                             components.add(componentIds[i]);
                         }
                         issueService.setIssueComponents(issue.getId(), components, creator);
                     }
                     HashSet<Integer> versions = new HashSet<Integer>();
-                    Integer[] versionIds = (Integer[]) PropertyUtils.getSimpleProperty(form, "versions");
-                    if(versionIds != null) {
+                    Integer[] versionIds = issueForm.getVersions();
+                    
+                    if (versionIds != null) {
                         for(int i = 0; i < versionIds.length; i++) {
                             versions.add(versionIds[i]);
                         }
@@ -184,38 +204,50 @@ public class CreateIssueAction extends ItrackerBaseAction {
                     }
                     
                     if(! ProjectUtilities.hasOption(ProjectUtilities.OPTION_NO_ATTACHMENTS, project.getOptions())) {
-                        FormFile file = (FormFile) PropertyUtils.getSimpleProperty(form, "attachment");
-                        if(file != null && ! "".equals(file.getFileName())) {
+                        FormFile file = issueForm.getAttachment();
+                        
+                        if (file != null && ! "".equals(file.getFileName())) {
                             String origFileName = file.getFileName();
-                            if(AttachmentUtilities.checkFile(file, this.getITrackerServices())) {
+                            
+                            if (AttachmentUtilities.checkFile(file, this.getITrackerServices())) {
                                 int lastSlash = Math.max(origFileName.lastIndexOf('/'), origFileName.lastIndexOf('\\'));
-                                if(lastSlash > -1) {
+                                
+                                if (lastSlash > -1) {
                                     origFileName = origFileName.substring(lastSlash + 1);
                                 }
                                 
-                                IssueAttachment attachmentModel = new IssueAttachment(origFileName,
+                                IssueAttachment attachmentModel =
+                                        new IssueAttachment(issue, origFileName,
                                         file.getContentType(),
-                                        (String) PropertyUtils.getSimpleProperty(form, "attachmentDescription"),
-                                        file.getFileSize(), issue, currUser);
+                                        issueForm.getAttachmentDescription(),
+                                        file.getFileSize(), currUser);
                                 issueService.addIssueAttachment(attachmentModel, file.getFileData());
                             }
                         }
                     }
                     
                     try {
-                        Integer relatedIssueId = (Integer) PropertyUtils.getSimpleProperty(form, "relatedIssueId");
-                        Integer relationType = (Integer) PropertyUtils.getSimpleProperty(form, "relationType");
-                        if(relatedIssueId != null && relatedIssueId.intValue() > 0 && relationType != null && relationType.intValue() > 0) {
+                        Integer relatedIssueId = issueForm.getRelatedIssueId();
+                        Integer relationType = issueForm.getRelationType();
+                        
+                        if (relatedIssueId != null && relatedIssueId.intValue() > 0
+                                && relationType != null && relationType.intValue() > 0) {
                             Issue relatedIssue = issueService.getIssue(relatedIssueId);
+                            
                             if(relatedIssue == null) {
                                 logger.debug("Unknown relation issue, relation not created.");
-                            } else if(relatedIssue.getProject() == null || ! IssueUtilities.canEditIssue(relatedIssue, currUserId, userPermissionsMap)) {
-                                logger.info("User not authorized to add issue relation from issue " + issue.getId() + " to issue " + relatedIssueId);
+                            } else if(relatedIssue.getProject() == null
+                                    || ! IssueUtilities.canEditIssue(relatedIssue, currUserId, userPermissionsMap)) {
+                                logger.info("User not authorized to add issue relation from issue "
+                                        + issue.getId() + " to issue " + relatedIssueId);
                             } else if(IssueUtilities.hasIssueRelation(issue, relatedIssueId)) {
-                                logger.debug("Issue " + issue.getId() + " is already related to issue " + relatedIssueId + ", relation ot created.");
+                                logger.debug("Issue " + issue.getId()
+                                + " is already related to issue " + relatedIssueId + ", relation ot created.");
                             } else {
-                                if(! issueService.addIssueRelation(issue.getId(), relatedIssueId, relationType.intValue(), currUser.getId())) {
-                                    logger.info("Error adding issue relation from issue " + issue.getId() + " to issue " + relatedIssueId);
+                                if(! issueService.addIssueRelation(issue.getId(),
+                                        relatedIssueId, relationType.intValue(), currUser.getId())) {
+                                    logger.info("Error adding issue relation from issue "
+                                            + issue.getId() + " to issue " + relatedIssueId);
                                 }
                             }
                         }
@@ -227,7 +259,7 @@ public class CreateIssueAction extends ItrackerBaseAction {
                 }
                 session.removeAttribute(Constants.PROJECT_KEY);
                 
-                WorkflowUtilities.ProcessFieldScripts(scripts, WorkflowUtilities.EVENT_FIELD_ONPOSTSUBMIT, null, errors, (ValidatorForm) form);
+                WorkflowUtilities.processFieldScripts(scripts, WorkflowUtilities.EVENT_FIELD_ONPOSTSUBMIT, null, errors, (ValidatorForm) form);
                 
                 request.setAttribute("projects",projectService.getAllProjects());
                 request.setAttribute("ph",projectService);
@@ -237,12 +269,12 @@ public class CreateIssueAction extends ItrackerBaseAction {
                 
                 // return mapping.findForward("listissues");
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             logger.error("Exception processing form data", e);
             errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("itracker.web.error.system"));
         }
         
-        if(! errors.isEmpty()) {
+        if (!errors.isEmpty()) {
             saveMessages(request, errors);
         }
         return mapping.findForward("error");
