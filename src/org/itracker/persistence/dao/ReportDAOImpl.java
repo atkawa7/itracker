b22a594ea7dcc4caac495018b9df51dbd6bdd98d
johnnymac75 <johnnymac75@users.sf.net>
2006-12-25 13:19:31 +0000

    - Converted all queries to named HQL queries - Reviewed all queries with iTracker 2 ejb-jar.xml

--- src/org/itracker/persistence/dao/ReportDAOImpl.java
+++ src/org/itracker/persistence/dao/ReportDAOImpl.java
@@ -1,31 +1,34 @@
 package org.itracker.persistence.dao;
 
 import java.util.List;
-
-import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.itracker.model.Report;
  
 /**
  * 
  */
-public class ReportDAOImpl extends BaseHibernateDAOImpl<Report> implements ReportDAO {
+public class ReportDAOImpl extends BaseHibernateDAOImpl<Report> 
+        implements ReportDAO {
 
     public Report findByPrimaryKey(Integer id) {
+        Report report;
+        
         try {
-            return (Report)getSession().get(Report.class, id);
+            report = (Report)getSession().get(Report.class, id);
         } catch (HibernateException ex) {
             throw convertHibernateAccessException(ex);
         }
+        return report;
     }
 
     @SuppressWarnings("unchecked")
     public List<Report> findAll() {
         final List<Report> reports;
         
-        Criteria criteria = getSession().createCriteria(Report.class);
         try {
-            reports = criteria.list();
+            Query query = getSession().getNamedQuery("ReportsAllQuery");
+            reports = query.list();
         } catch (HibernateException ex) {
             throw convertHibernateAccessException(ex);
         }
