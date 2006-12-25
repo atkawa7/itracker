e3c8ea45aa0fd85012632db0092f3e02216a8b98
johnnymac75 <johnnymac75@users.sf.net>
2006-12-25 13:55:27 +0000

    Removed abstract method already that are part of the implemented interfaces Used Map instead of HashMap

--- src/org/itracker/services/authentication/AbstractPluggableAuthenticator.java
+++ src/org/itracker/services/authentication/AbstractPluggableAuthenticator.java
@@ -18,12 +18,9 @@
 
 package org.itracker.services.authentication;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.itracker.model.Permission;
-import org.itracker.model.User;
 import org.itracker.services.ConfigurationService;
 import org.itracker.services.UserService;
 import org.itracker.services.exceptions.AuthenticatorException;
@@ -47,165 +44,10 @@ public abstract class AbstractPluggableAuthenticator
     }
 
     /**
-      * This method should be overridden to determine if a user login is successful.  The method
-      * should return a valid User object.
-      * @param login the login the user/client provided
-      * @param authentication the user's authentication information, if known
-      * @param authType the type of authentication information being provided
-      * @param reqSource the source of the request (eg web, api)
-      * @return a User if the login is successful
-      * @throws AuthenticatorException an exception if the login is unsuccessful, or an error occurs
-      */
-    public abstract User checkLogin(String login, Object authentication, int authType, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be overridden to return all the permissions a user has in the
-      * authentication system.  This list may then be augmented based on other attributes of
-      * the user, or project level options.
-      * @param user a User object that contains the user to retrieve permissions for
-      * @param reqSource the source of the request (eg web, api)
-      * @return an array of PermissionModels
-      * @throws AuthenticatorException an error occurs
-      */
-    public abstract List<Permission> getUserPermissions(User user, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be overridden to return an array of users that have certian permissions
-      * in the authentication system.  This list must always include all super users, even if they
-      * do not explicitly have the required permission.
-      * @param permissions an array of PermissionModels that define which permissions in which
-               projects are required.
-      * @param requireAll true if the user must possess any of the permissions, false if only one is required
-      * @param activeOnly true if only users listed as active should be returned
-      * @param reqSource the source of the request (eg web, api)
-      * @return an array of UserModels
-      * @throws AuthenticatorException an error occurs
-      */
-    public abstract List<User> getUsersWithProjectPermission(List<Permission> permissions, boolean requireAll, boolean activeOnly, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be overridden to determine if a user is authorized to self register.
-      * @param user a User object that contains the data the user submitted
-      * @param authentication the user's authentication information, if known
-      * @param authType the type of authentication information being provided
-      * @param reqSource the source of the request (eg web, api)
-      * @return a boolean whether the user should be allowed to register
-      * @throws AuthenticatorException an exception if an error occurs
-      */
-    public abstract boolean allowRegistration(User user, Object authentication, int authType, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be overwritten to return if any new user profile should be allowed
-      * to be created.
-      * @param user a User object that contains the data for the new user.  If null,
-               then the request is being made for an unknown future user.  For example,
-               the system may request this with an null user if it needs to know if the system
-               should even present the option to create a new user
-      * @param authentication the user's authentication information, if known
-      * @param authType the type of authentication information being provided
-      * @param reqSource the source of the request (eg web, api)
-      * @return a boolean whether the new profile creation is allowed
-      * @throws AuthenticatorException an exception if an error occurs
-      */
-    public abstract boolean allowProfileCreation(User user, Object authentication, int authType, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be overridden to return if the particular user can have
-      * core profile information updates on the system.
-      * @param user a User object that contains the current user data
-      * @param authentication the user's authentication information, if known
-      * @param authType the type of authentication information being provided
-      * @param reqSource the source of the request (eg web, api)
-      * @return a boolean whether the user's core profile information can be updated
-      * @throws AuthenticatorException an exception if an error occurs
-      * @see PluggableAuthenticator#allowPasswordUpdates
-      * @see PluggableAuthenticator#allowPermissionUpdates
-      * @see PluggableAuthenticator#allowPreferenceUpdates
-      */
-    public abstract boolean allowProfileUpdates(User user, Object authentication, int authType, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be overridden to return if the particular user profile can have
-      * password updates on the system.
-      * @param user a User object that contains the current user data
-      * @param authentication the user's authentication information, if known
-      * @param authType the type of authentication information being provided
-      * @param reqSource the source of the request (eg web, api)
-      * @return a boolean whether the user's core profile information can be updated
-      * @throws AuthenticatorException an exception if an error occurs
-      * @see PluggableAuthenticator#allowProfileUpdates
-      * @see PluggableAuthenticator#allowPermissionUpdates
-      * @see PluggableAuthenticator#allowPreferenceUpdates
-      */
-    public abstract boolean allowPasswordUpdates(User user, Object authentication, int authType, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be overridden to return if the particular user profile can have
-      * permission updates on the system.
-      * information is allowed to be updated through ITracker.
-      * @param user a User object that contains the current user data, or null if multiple users
-      * @param authentication the user's authentication information, if known
-      * @param authType the type of authentication information being provided
-      * @param reqSource the source of the request (eg web, api)
-      * @return a boolean whether the user's core profile information can be updated
-      * @throws AuthenticatorException an exception if an error occurs
-      * @see PluggableAuthenticator#allowProfileUpdates
-      * @see PluggableAuthenticator#allowPasswordUpdates
-      * @see PluggableAuthenticator#allowPreferenceUpdates
-      */
-    public abstract boolean allowPermissionUpdates(User user, Object authentication, int authType, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be implemented to determine if the particular user can have
-      * preferences updates on the system.
-      * @param user a User object that contains the current user data
-      * @param authentication the user's authentication information, if known
-      * @param authType the type of authentication information being provided
-      * @param reqSource the source of the request (eg web, api)
-      * @return a boolean whether the user's core profile information can be updated
-      * @throws AuthenticatorException an exception if an error occurs
-      * @see PluggableAuthenticator#allowProfileUpdates
-      * @see PluggableAuthenticator#allowPasswordUpdates
-      * @see PluggableAuthenticator#allowPermissionUpdates
-      */
-    public abstract boolean allowPreferenceUpdates(User user, Object authentication, int authType, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be overridden to perform any updates that are necessary in the authentication
-      * system to support a new user.  Any changes should be made directly to user model supplied to the
-      * method.  The system will then update the information in the ITracker datastore.  Only changes to the
-      * core profile information and password are made here.  Any permission information for the new user
-      * whould be done through an updateProfile call.
-      * @param user a User object that contains the profile information to be created
-      * @param authentication the user's authentication information, if known
-      * @param authType the type of authentication information being provided
-      * @param reqSource the source of the request (eg web, api)
-      * @return true if changes were made
-      * @throws AuthenticatorException an error occurs
-      * @see PluggableAuthenticator#updateProfile
-      */
-    public abstract boolean createProfile(User user, Object authentication, int authType, int reqSource) throws AuthenticatorException;
-
-    /**
-      * This method should be overridden to perform any updates that are necessary in the authentication
-      * system to support the updated user information.  This action will be called any time there are any
-      * updates to a user includeing core profile information, password information, permission information
-      * or preference changes.  Any changes should be made directly to user model supplied to the method.
-      * @param user a User object that contains the updated profile
-      * @param updateType the type of information that is being updated
-      * @param authentication the user's authentication information, if known
-      * @param authType the type of authentication information being provided
-      * @param reqSource the source of the request (eg web, api)
-      * @return true if changes were made
-      * @throws AuthenticatorException an exception if the login is unsuccessful, or an error occurs
-      */
-    public abstract boolean updateProfile(User user, int updateType, Object authentication, int authType, int reqSource) throws AuthenticatorException;
-
-    /**
       * This method is called after creating a new instance of the Authenticator.  It supplies
       * some default EJB objects that the authenticator can use.
       */
-    public void initialize(HashMap values) {
+    public void initialize(Map values) {
         if(values != null) {
             Object userService = values.get("userService");
             Object configurationService = values.get("configurationService");
@@ -247,4 +89,5 @@ public abstract class AbstractPluggableAuthenticator
 
         return configurationService;
     }
+    
 }
