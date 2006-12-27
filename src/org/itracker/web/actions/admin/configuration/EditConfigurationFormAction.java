f5f8518a98e0bd0a3602d8de00bdbbdf69316a3d
MarkyGoldstein <MarkyGoldstein@users.sf.net>
2006-12-27 09:52:18 +0000

    removed not used imports.

--- src/org/itracker/web/actions/admin/configuration/EditConfigurationFormAction.java
+++ src/org/itracker/web/actions/admin/configuration/EditConfigurationFormAction.java
@@ -27,7 +27,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
