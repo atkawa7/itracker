9adf2c5ea6e412abba1c21d85af20c34c690eef9
johnnymac75 <johnnymac75@users.sf.net>
2007-01-05 14:56:12 +0000

    Added Hibernate custom types to persist Java 5 enums

---
+++ src/org/itracker/persistence/dao/EnumUserType.java
@@ -0,0 +1,67 @@
+package org.itracker.persistence.dao;
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Properties;
+
+import org.hibernate.HibernateException;
+import org.hibernate.usertype.EnhancedUserType;
+import org.hibernate.usertype.ParameterizedType;
+
+/**
+ * Custom Hibernate UserType to persist a Java 5 enum constant as a VARCHAR
+ * using its name. 
+ * 
+ * <p>Based on the original class by Gavin King 
+ * (http://www.hibernate.org/272.html). </p>
+ * 
+ * @author johnny
+ */
+public class EnumUserType extends AbstractEnumUserType {
+    
+    private static final int[] SQL_TYPES = { Types.VARCHAR };
+
+    /** 
+     * Default constructor, required by Hibernate. 
+     */
+    public EnumUserType() {
+    }
+    
+    @SuppressWarnings("unchecked")
+    public Object nullSafeGet(ResultSet rs, String[] names, Object owner) 
+    throws HibernateException, SQLException {
+        final String name = rs.getString(names[0]);
+        
+        return rs.wasNull() ? null : Enum.valueOf(this.enumClass, name);
+    }
+
+    public void nullSafeSet(PreparedStatement st, Object value, int index) 
+    throws HibernateException, SQLException {
+        if (value == null) {
+            st.setNull(index, Types.VARCHAR);
+        } else {
+            st.setString(index, ((Enum) value).name()); 
+        }
+    }
+
+    public int[] sqlTypes() {
+        return SQL_TYPES;
+    }
+
+    public String objectToSQLString(Object value) {
+        return '\'' + ((Enum) value).name() + '\'';
+    }
+
+    public String toXMLString(Object value) {
+        return ((Enum) value).name();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public Object fromXMLString(String xmlValue) {
+        return Enum.valueOf(this.enumClass, xmlValue);
+    }
+
+}
