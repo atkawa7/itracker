96dfabb3517c0746fda406607e35d89e1f3afc6f
johnnymac75 <johnnymac75@users.sf.net>
2007-01-06 10:52:53 +0000

    javadoc

--- src/org/itracker/model/Entity.java
+++ src/org/itracker/model/Entity.java
@@ -3,23 +3,41 @@ package org.itracker.model;
 import java.io.Serializable;
 
 /**
- * A database entity. 
+ * All database entities must implement this interface, which represents 
+ * the minimum requirements to meet. 
+ * 
+ * <p>A database entity always has an <code>Integer</code> surrogate key 
+ * (system ID). It must also be Serializable and Cloneable. </p>
+ * 
+ * <p>A database entity class must also fulfill the following requirements, 
+ * that cannot be expressed by a Java interface : </p>
+ * <ul>
+ *  <li>be a Java bean ; </li>
+ *  <li>be non-final. </li>
+ * </ul>
  * 
  * @author johnny
+ * @see AbstractEntity
  */
 public interface Entity extends Serializable, Cloneable {
+    
+    /* PENDING : should createDate and lastModifiedDate properties 
+     * be part of this interface ? 
+     * Should we add createdBy and lastModifiedBy ? */
     
     /**
      * Returns the system ID. 
      * 
-     * @return ID > 0 for persistent entities, <tt>null</tt> for transient ones
+     * @return ID &gt; 0 for persistent entities, 
+     *         <tt>null</tt> for transient ones
      */
     Integer getId();
     
     /**
      * Sets this entity's system ID. 
      * 
-     * @param id ID > 0 for persistent entities, <tt>null</tt> for transient ones
+     * @param id ID &gt; 0 for persistent entities, 
+     *        <tt>null</tt> for transient ones
      */
     void setId(Integer id);
     
