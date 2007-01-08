64c464b3cd5a17346edfc1841c014d87e276e2a7
MarkyGoldstein <MarkyGoldstein@users.sf.net>
2007-01-08 23:03:15 +0000

    deleted deprecatedmodels package.

--- src/org/itracker/model/deprecatedmodels/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains old data transfer objects representing model objects.  
- * These classes are no longer used and have all been declared package private 
- * to prevent anybody from using them. 
- *
- * This package is kept for reference until the refactoring is complete. 
- */
-package org.itracker.model.deprecatedmodels;
