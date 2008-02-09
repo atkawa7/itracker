d20d8002c3efdfdd3bc123115c095bb04dec9dd8
clim1219 <clim1219@users.sf.net>
2008-02-09 03:20:47 +0000

    Code cleanup before refactoring.

--- src/main/java/org/itracker/web/actions/admin/AdminHomeAction.java
+++ src/main/java/org/itracker/web/actions/admin/AdminHomeAction.java
@@ -1,10 +1,9 @@
 package org.itracker.web.actions.admin;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -22,54 +21,63 @@ import org.itracker.web.util.SessionManager;
 
 public class AdminHomeAction extends ItrackerBaseAction {
 
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		super.executeAlways(mapping,form,request,response);
-		  IssueService issueService = this.getITrackerServices().getIssueService();
-		  ProjectService projectService = this.getITrackerServices().getProjectService();
-		  ReportService reportService = this.getITrackerServices().getReportService();
-		  ConfigurationService configurationService = this.getITrackerServices().getConfigurationService();
-		  UserService userService = this.getITrackerServices().getUserService();
-		  Integer numberOfWorkflowScripts = configurationService.getWorkflowScripts().size();
-		  request.setAttribute("numberOfWorkflowScripts",numberOfWorkflowScripts);
-		  Integer numberDefinedKeys = configurationService.getNumberDefinedKeys(null);
-		  request.setAttribute("numberDefinedKeys",numberDefinedKeys);
-		  Integer numberOfStatuses = configurationService.getConfigurationItemsByType(SystemConfigurationUtilities.TYPE_STATUS).size();
-		  request.setAttribute("numberOfStatuses",numberOfStatuses);
-		  Integer numberOfSeverities = configurationService.getConfigurationItemsByType(SystemConfigurationUtilities.TYPE_SEVERITY).size();
-		  request.setAttribute("numberOfSeverities",numberOfSeverities);
-		  Integer numberOfResolutions = configurationService.getConfigurationItemsByType(SystemConfigurationUtilities.TYPE_RESOLUTION).size();
-		  request.setAttribute("numberOfResolutions",numberOfResolutions);
-		  Integer numberOfCustomProjectFields = configurationService.getCustomFields().size();
-		  request.setAttribute("numberOfCustomProjectFields",numberOfCustomProjectFields);
-		  int numberofActiveSesssionsString = SessionManager.getNumActiveSessions();
-		  Integer numberofActiveSesssions = new Integer(numberofActiveSesssionsString);
-		  request.setAttribute("numberofActiveSesssions",numberofActiveSesssions);
-		  Integer allIssueAttachmentsTotalNumber = issueService.countSystemIssuesAttachments();
-		  Long allIssueAttachmentsTotalSize = new Long(issueService.totalSystemIssuesAttachmentSize());
-		  request.setAttribute("allIssueAttachmentsTotalNumber",allIssueAttachmentsTotalNumber);
-		  request.setAttribute("allIssueAttachmentsTotalSize",allIssueAttachmentsTotalSize);
-		  Locale currLocale = super.getLocale(request);
-		  SimpleDateFormat sdf = new SimpleDateFormat(ITrackerResources.getString("itracker.dateformat.full"), currLocale);
-		  String lastRun = null;//(Scheduler.getLastRun() == null ? "-" : sdf.format(Scheduler.getLastRun()));
-		  request.setAttribute("lastRun",lastRun);
-		  request.setAttribute("ih",issueService);
-		  request.setAttribute("ph",projectService);
-		  request.setAttribute("rh",reportService);
-		  request.setAttribute("sc",configurationService);
-		  request.setAttribute("uh",userService);
-		  
-		  String pageTitleKey = "itracker.web.admin.index.title"; 
-	      String pageTitleArg = "";			
-	      request.setAttribute("pageTitleKey",pageTitleKey); 
-	      request.setAttribute("pageTitleArg",pageTitleArg); 
-		  
-		return mapping.findForward("adminhome");
-	}
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+            throws Exception {
 
-	public AdminHomeAction() {
-		super();
-	}
+        super.executeAlways(mapping, form, request, response);
+
+        IssueService issueService = this.getITrackerServices().getIssueService();
+        ProjectService projectService = this.getITrackerServices().getProjectService();
+        ReportService reportService = this.getITrackerServices().getReportService();
+        ConfigurationService configurationService = this.getITrackerServices().getConfigurationService();
+        UserService userService = this.getITrackerServices().getUserService();
+        Integer numberOfWorkflowScripts = configurationService.getWorkflowScripts().size();
+        request.setAttribute("numberOfWorkflowScripts", numberOfWorkflowScripts);
+
+        Integer numberDefinedKeys = configurationService.getNumberDefinedKeys(null);
+        request.setAttribute("numberDefinedKeys", numberDefinedKeys);
+
+        Integer numberOfStatuses = configurationService.getConfigurationItemsByType(SystemConfigurationUtilities.TYPE_STATUS).size();
+        request.setAttribute("numberOfStatuses", numberOfStatuses);
+
+        Integer numberOfSeverities = configurationService.getConfigurationItemsByType(SystemConfigurationUtilities.TYPE_SEVERITY).size();
+        request.setAttribute("numberOfSeverities", numberOfSeverities);
+
+        Integer numberOfResolutions = configurationService.getConfigurationItemsByType(SystemConfigurationUtilities.TYPE_RESOLUTION).size();
+        request.setAttribute("numberOfResolutions", numberOfResolutions);
+
+        Integer numberOfCustomProjectFields = configurationService.getCustomFields().size();
+        request.setAttribute("numberOfCustomProjectFields", numberOfCustomProjectFields);
+
+        Integer numberofActiveSesssions = SessionManager.getNumActiveSessions();
+        request.setAttribute("numberofActiveSesssions", numberofActiveSesssions);
+
+        Integer allIssueAttachmentsTotalNumber = issueService.countSystemIssuesAttachments();
+        request.setAttribute("allIssueAttachmentsTotalNumber", allIssueAttachmentsTotalNumber);
+
+        Long allIssueAttachmentsTotalSize = issueService.totalSystemIssuesAttachmentSize();
+        request.setAttribute("allIssueAttachmentsTotalSize", allIssueAttachmentsTotalSize);
+
+        Locale currLocale = super.getLocale(request);
+        SimpleDateFormat sdf = new SimpleDateFormat(ITrackerResources.getString("itracker.dateformat.full"), currLocale);
+        String lastRun = null;//(Scheduler.getLastRun() == null ? "-" : sdf.format(Scheduler.getLastRun()));
+
+        request.setAttribute("lastRun", lastRun);
+        request.setAttribute("ih", issueService);
+        request.setAttribute("ph", projectService);
+        request.setAttribute("rh", reportService);
+        request.setAttribute("sc", configurationService);
+        request.setAttribute("uh", userService);
+
+        String pageTitleKey = "itracker.web.admin.index.title";
+        String pageTitleArg = "";
+        request.setAttribute("pageTitleKey", pageTitleKey);
+        request.setAttribute("pageTitleArg", pageTitleArg);
+
+        return mapping.findForward("adminhome");
+    }
 
 }
