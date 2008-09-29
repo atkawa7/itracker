23ffcfdff3c03684969b51fd723ce34562ec4b84
ranks <ranks@users.sf.net>
2008-09-29 15:28:23 +0000

    performance optimizing: - don't count issueattachments total size if there are more than 500 attachments(prox. 3 sec per 1000 attachments)

--- src/main/java/org/itracker/web/actions/admin/AdminHomeAction.java
+++ src/main/java/org/itracker/web/actions/admin/AdminHomeAction.java
@@ -1,10 +1,12 @@
 package org.itracker.web.actions.admin;
 
-import java.util.Locale;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -20,46 +22,66 @@ import org.itracker.web.util.SessionManager;
 
 public class AdminHomeAction extends ItrackerBaseAction {
 
+	private static final Logger log = Logger.getLogger(AdminHomeAction.class);
     public ActionForward execute(ActionMapping mapping,
                                  ActionForm form,
                                  HttpServletRequest request,
                                  HttpServletResponse response)
             throws Exception {
 
+    	Date time_millies = new Date(System.currentTimeMillis());
 //        super.executeAlways(mapping, form, request, response);
 
+    	
         IssueService issueService = this.getITrackerServices().getIssueService();
         ProjectService projectService = this.getITrackerServices().getProjectService();
         ReportService reportService = this.getITrackerServices().getReportService();
         ConfigurationService configurationService = this.getITrackerServices().getConfigurationService();
         UserService userService = this.getITrackerServices().getUserService();
+        
+        logTimeMillies("execute: looked up services", time_millies, log, Level.INFO);
         Integer numberOfWorkflowScripts = configurationService.getWorkflowScripts().size();
         request.setAttribute("numberOfWorkflowScripts", numberOfWorkflowScripts);
+        logTimeMillies("execute: looked up numberOfWorkflowScripts", time_millies, log, Level.INFO);
 
         Integer numberDefinedKeys = configurationService.getNumberDefinedKeys(null);
         request.setAttribute("numberDefinedKeys", numberDefinedKeys);
+        logTimeMillies("execute: looked up numberDefinedKeys", time_millies, log, Level.INFO);
 
         Integer numberOfStatuses = configurationService.getConfigurationItemsByType(SystemConfigurationUtilities.TYPE_STATUS).size();
         request.setAttribute("numberOfStatuses", numberOfStatuses);
+        logTimeMillies("execute: looked up numberOfStatuses", time_millies, log, Level.INFO);
 
         Integer numberOfSeverities = configurationService.getConfigurationItemsByType(SystemConfigurationUtilities.TYPE_SEVERITY).size();
         request.setAttribute("numberOfSeverities", numberOfSeverities);
+        logTimeMillies("execute: looked up numberOfSeverities", time_millies, log, Level.INFO);
 
         Integer numberOfResolutions = configurationService.getConfigurationItemsByType(SystemConfigurationUtilities.TYPE_RESOLUTION).size();
         request.setAttribute("numberOfResolutions", numberOfResolutions);
+        logTimeMillies("execute: looked up numberOfResolutions", time_millies, log, Level.INFO);
 
         Integer numberOfCustomProjectFields = configurationService.getCustomFields().size();
         request.setAttribute("numberOfCustomProjectFields", numberOfCustomProjectFields);
+        logTimeMillies("execute: looked up numberOfCustomProjectFields", time_millies, log, Level.INFO);
 
         Integer numberofActiveSesssions = SessionManager.getNumActiveSessions();
         request.setAttribute("numberofActiveSesssions", numberofActiveSesssions);
+        logTimeMillies("execute: looked up numberofActiveSesssions", time_millies, log, Level.INFO);
 
         Long allIssueAttachmentsTotalNumber = issueService.countSystemIssuesAttachments();
         request.setAttribute("allIssueAttachmentsTotalNumber", allIssueAttachmentsTotalNumber);
+        logTimeMillies("execute: looked up allIssueAttachmentsTotalNumber", time_millies, log, Level.INFO);
 
-        Long allIssueAttachmentsTotalSize = issueService.totalSystemIssuesAttachmentSize();
-        request.setAttribute("allIssueAttachmentsTotalSize", allIssueAttachmentsTotalSize);
-
+        // TODO: performance issue when attachments size needs to be calculated over many issues !
+//        select sum(size)
+//        from IssueAttachment
+        if (allIssueAttachmentsTotalNumber < 500) {
+	        Long allIssueAttachmentsTotalSize = issueService.totalSystemIssuesAttachmentSize();
+	        request.setAttribute("allIssueAttachmentsTotalSize", allIssueAttachmentsTotalSize);
+        } else {
+	        request.setAttribute("allIssueAttachmentsTotalSize", -1l);
+        }
+        logTimeMillies("execute: looked up allIssueAttachmentsTotalSize", time_millies, log, Level.INFO);
 //        Locale locale = getCurrLocale(request);
 //        SimpleDateFormat sdf = new SimpleDateFormat(ITrackerResources.getString("itracker.dateformat.full"), locale);
         String lastRun = null;//(Scheduler.getLastRun() == null ? "-" : sdf.format(Scheduler.getLastRun()));
@@ -70,12 +92,14 @@ public class AdminHomeAction extends ItrackerBaseAction {
         request.setAttribute("rh", reportService);
         request.setAttribute("sc", configurationService);
         request.setAttribute("uh", userService);
+        logTimeMillies("execute: put services to request", time_millies, log, Level.INFO);
 
         String pageTitleKey = "itracker.web.admin.index.title";
         String pageTitleArg = "";
         request.setAttribute("pageTitleKey", pageTitleKey);
         request.setAttribute("pageTitleArg", pageTitleArg);
 
+        logTimeMillies("execute: returning", time_millies, log, Level.INFO);
         return mapping.findForward("adminhome");
     }
 
