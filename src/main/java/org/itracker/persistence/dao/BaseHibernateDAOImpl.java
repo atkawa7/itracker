26e808c2d1731f202e813083b0d55f95c277cd01
ranks <ranks@users.sf.net>
2008-10-16 10:42:08 +0000

    migrated setting of create/modified date to BaseHibernateDAO save/saveOrUpdate

--- src/main/java/org/itracker/persistence/dao/BaseHibernateDAOImpl.java
+++ src/main/java/org/itracker/persistence/dao/BaseHibernateDAOImpl.java
@@ -1,6 +1,7 @@
 package org.itracker.persistence.dao;
 
 import java.sql.Connection;
+import java.util.Date;
 
 import org.hibernate.HibernateException;
 import org.itracker.model.Entity;
@@ -21,8 +22,17 @@ public abstract class BaseHibernateDAOImpl<T extends Entity> extends HibernateDa
         super();
     }
 
+    /**
+     * insert a new entity.
+     * create- and lastmodified-date is set with current time.
+     * 
+     * @param entity - detached entity object
+     */
     public void save(T entity) {
         try {
+        	Date createDate = new Date();
+        	entity.setCreateDate(createDate);
+        	entity.setLastModifiedDate(createDate);
             getSession().save(entity);
         } catch (HibernateException ex) {
             throw convertHibernateAccessException(ex);
@@ -30,10 +40,19 @@ public abstract class BaseHibernateDAOImpl<T extends Entity> extends HibernateDa
     }
 
     /**
+     * inserts a new detached entity or updates if it already exists.
+     * create- and update-date are set automatically.
      * 
+     * @param entity - entity object to be inserted or updated
      */
     public void saveOrUpdate(T entity) {
         try {
+        	if (null == entity.getCreateDate()){
+        		entity.setCreateDate(new Date());
+        		entity.setLastModifiedDate(entity.getCreateDate());
+        	} else {
+        		entity.setLastModifiedDate(new Date());
+        	}
             getSession().saveOrUpdate(entity);
         } catch (HibernateException ex) {
             throw convertHibernateAccessException(ex);
@@ -43,8 +62,6 @@ public abstract class BaseHibernateDAOImpl<T extends Entity> extends HibernateDa
     public void delete(T entity) {
         try {
             getSession().delete(entity);
-            
-
         } catch (HibernateException ex) {
             throw convertHibernateAccessException(ex);
         }    
