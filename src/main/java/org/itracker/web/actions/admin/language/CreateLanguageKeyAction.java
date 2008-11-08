e8abb2ce478878f3b162b724b762eb590bc433ae
MarkyGoldstein <MarkyGoldstein@users.sf.net>
2008-11-08 19:50:59 +0000

    removed unused import.

--- src/main/java/org/itracker/web/actions/admin/language/CreateLanguageKeyAction.java
+++ src/main/java/org/itracker/web/actions/admin/language/CreateLanguageKeyAction.java
@@ -28,7 +28,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.log4j.Logger;
-import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
