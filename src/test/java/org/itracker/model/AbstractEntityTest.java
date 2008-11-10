16cb4e77539b9a9837e58991f43328ff3955e8f0
ranks <ranks@users.sf.net>
2008-11-10 15:04:48 +0000

    fixing error no runnables found (TestAbstractEntity moved to inner private class of testing class)

--- src/test/java/org/itracker/model/AbstractEntityTest.java
+++ src/test/java/org/itracker/model/AbstractEntityTest.java
@@ -2,52 +2,61 @@ package org.itracker.model;
 
 import java.util.Date;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class AbstractEntityTest extends TestCase {
 	private AbstractEntity ae;
-	
-	public void testGetCreateDate(){
+
+	@Test
+	public void testGetCreateDate() {
 		Date date = new Date(1000);
 		ae.setCreateDate(date);
 		assertEquals("create date", date, ae.getCreateDate());
-		
+
 		ae.setCreateDate(null);
 		assertNotNull("create date", ae.getCreateDate());
 	}
-	
-	public void testSetCreateDate(){
+
+	@Test
+	public void testSetCreateDate() {
 		Date date = new Date(1000);
 		ae.setCreateDate(date);
 		assertEquals("create date", date, ae.getCreateDate());
-		
+
 		ae.setCreateDate(null);
 		assertNotNull("create date", ae.getCreateDate());
 	}
-	
-	public void testGetLastModifiedDate(){
+
+	@Test
+	public void testGetLastModifiedDate() {
 		Date date = new Date(1000);
 		ae.setLastModifiedDate(date);
 		assertEquals("LastModifiedDate", date, ae.getLastModifiedDate());
-		
+
 		ae.setLastModifiedDate(null);
 		assertNotNull("LastModifiedDate", ae.getLastModifiedDate());
 	}
-	
-	public void testSetLastModifiedDate(){
+
+	@Test
+	public void testSetLastModifiedDate() {
 		Date date = new Date(1000);
 		ae.setLastModifiedDate(date);
 		assertEquals("LastModifiedDate", date, ae.getLastModifiedDate());
-		
+
 		ae.setLastModifiedDate(null);
 		assertNotNull("LastModifiedDate", ae.getLastModifiedDate());
 	}
-	
-	public void testEquals(){
+
+	@Test
+	public void testEquals() {
 		AbstractEntity aeCopy = ae;
 		assertTrue(ae.equals(aeCopy));
 		assertFalse(ae.equals(null));
-				
+
 		aeCopy = new TestAbstractEntity();
 		assertFalse(ae.equals(aeCopy));
 		ae.setId(1000);
@@ -56,46 +65,58 @@ public class AbstractEntityTest extends TestCase {
 		assertTrue(ae.equals(aeCopy));
 		assertFalse(ae.equals(new User()));
 	}
-	
-	public void testCompareTo(){
+
+	@Test
+	public void testCompareTo() {
 		ae.setId(1000);
 		AbstractEntity aeCopy = new TestAbstractEntity();
 		aeCopy.setId(1000);
 		assertEquals(0, ae.compareTo(aeCopy));
 		aeCopy.setId(2000);
 		assertEquals(-1, ae.compareTo(aeCopy));
-		try{
-		 ae.compareTo(null);
+		try {
+			ae.compareTo(null);
 		} catch (IllegalArgumentException e) {
 			assertTrue(true);
 		}
-	}	
-	
-	public void testIsNew(){
+	}
+
+	@Test
+	public void testIsNew() {
 		ae.setId(1000);
 		assertFalse(ae.isNew());
 		ae.setId(null);
 		assertTrue(ae.isNew());
 	}
-	
-	public void testHashCode(){
+
+	@Test
+	public void testHashCode() {
 		assertNotNull(ae.hashCode());
 	}
-	
-	public void testClone() throws CloneNotSupportedException{
+
+	@Test
+	public void testClone() throws CloneNotSupportedException {
 		Object clone = ae.clone();
 		assertTrue(clone instanceof AbstractEntity);
 	}
-	
 
-	
-	@Override
-    protected void setUp() throws Exception {
+	@Before
+	protected void setUp() throws Exception {
 		ae = new TestAbstractEntity();
-    }
-	
-	@Override
+	}
+
+	@After
 	protected void tearDown() throws Exception {
 		ae = null;
 	}
+	
+	private class TestAbstractEntity extends AbstractEntity {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+	}
+
 }
--- src/test/java/org/itracker/model/TestAbstractEntity.java
+++
@@ -1,5 +0,0 @@
-package org.itracker.model;
-
-public class TestAbstractEntity extends AbstractEntity {
-
-}
