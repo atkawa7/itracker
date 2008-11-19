8b1beadef91ad6464e0145ed63cc0cfcd3ba55db
MarkyGoldstein <MarkyGoldstein@users.sf.net>
2008-11-19 21:24:57 +0000

    Remove ToDo for auto generated stub stuff

--- src/main/java/org/itracker/web/actions/ShowHelpAction.java
+++ src/main/java/org/itracker/web/actions/ShowHelpAction.java
@@ -57,7 +57,7 @@ public class ShowHelpAction extends ItrackerBaseAction {
     
     public ShowHelpAction() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
     
