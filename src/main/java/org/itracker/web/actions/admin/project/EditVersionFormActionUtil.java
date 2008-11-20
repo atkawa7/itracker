94d0d0045b662c9cb75a4d0724bab7551943e6ec
ranks <ranks@users.sf.net>
2008-11-20 10:40:44 +0000

    cleaning up loggings

--- src/main/java/org/itracker/web/actions/admin/project/EditVersionFormActionUtil.java
+++ src/main/java/org/itracker/web/actions/admin/project/EditVersionFormActionUtil.java
@@ -9,14 +9,25 @@ import org.itracker.model.Version;
 import org.itracker.web.util.Constants;
 
 public class EditVersionFormActionUtil {
-	private static final Logger log = Logger.getLogger(EditVersionFormActionUtil.class);
+	private static final Logger log = Logger
+			.getLogger(EditVersionFormActionUtil.class);
 
-	public ActionForward init(ActionMapping mapping, HttpServletRequest request){
-		log.debug("Getting version from session");
-		final Version version = (Version)request.getSession().getAttribute(Constants.VERSION_KEY);
-		log.debug("Checking if version is new");
+	public ActionForward init(ActionMapping mapping, HttpServletRequest request) {
+
+		if (log.isDebugEnabled()) {
+			log.debug("init: Getting version from session");
+		}
+		final Version version = (Version) request.getSession().getAttribute(
+				Constants.VERSION_KEY);
+		if (log.isDebugEnabled()) {
+			log.debug("init: Checking if version is new");
+		}
 		final boolean isNew = version.isNew();
-		log.debug("Putting the isNew="+isNew+"component"+version.getDescription()+" and isNew attribute back into the request");
+		if (log.isDebugEnabled()) {
+			log.debug("init: Putting the isNew=" + isNew + ", component :"
+					+ version.getDescription()
+					+ " and isNew attribute back into the request");
+		}
 		request.setAttribute("version", version);
 		request.setAttribute("isNew", isNew);
 		return null;
