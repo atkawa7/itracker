0a884bed78c99a96ec2dba3fedce32fb8c6033a6
MarkyGoldstein <MarkyGoldstein@users.sf.net>
2008-11-20 10:10:51 +0000

    adding some log.debug code to use the log ;) (to get rid of warning)

--- src/main/java/org/itracker/web/actions/admin/project/EditVersionFormActionUtil.java
+++ src/main/java/org/itracker/web/actions/admin/project/EditVersionFormActionUtil.java
@@ -12,8 +12,11 @@ public class EditVersionFormActionUtil {
 	private static final Logger log = Logger.getLogger(EditVersionFormActionUtil.class);
 
 	public ActionForward init(ActionMapping mapping, HttpServletRequest request){
+		log.debug("Getting version from session");
 		final Version version = (Version)request.getSession().getAttribute(Constants.VERSION_KEY);
+		log.debug("Checking if version is new");
 		final boolean isNew = version.isNew();
+		log.debug("Putting the isNew="+isNew+"component"+version.getDescription()+" and isNew attribute back into the request");
 		request.setAttribute("version", version);
 		request.setAttribute("isNew", isNew);
 		return null;
