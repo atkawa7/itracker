b8769fef44e92970bd64f490906188a9eb63d068
MarkyGoldstein <MarkyGoldstein@users.sf.net>
2008-11-20 10:18:59 +0000

    Removed unused import

--- src/main/java/org/itracker/web/actions/admin/project/EditProjectFormAction.java
+++ src/main/java/org/itracker/web/actions/admin/project/EditProjectFormAction.java
@@ -28,7 +28,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
