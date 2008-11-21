2fde6c28b1f70100acf19215529bd969e6c2d83f
MarkyGoldstein <MarkyGoldstein@users.sf.net>
2008-11-21 05:59:59 +0000

    Clean Up Action Code done

--- src/main/java/org/itracker/web/actions/ErrorAction.java
+++ src/main/java/org/itracker/web/actions/ErrorAction.java
@@ -7,17 +7,13 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.itracker.web.actions.base.ItrackerBaseAction;
-//  TODO: Action Cleanup
-
 
 public class ErrorAction extends ItrackerBaseAction {
 
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-//		 super.executeAlways(mapping,form,request,response);
-		// TODO process request and return an ActionForward instance, for example:
-		// return mapping.findForward("forward_name");
+
 		return mapping.findForward("error_page");
 	}
 
