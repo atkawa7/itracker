8456073e5f5b71b20816328b0ad5d47e8b1b5f82
MarkyGoldstein <MarkyGoldstein@users.sf.net>
2008-12-03 08:02:44 +0000

    improved script

--- src/main/webapp/themes/defaulttheme/includes/scripts.js
+++ src/main/webapp/themes/defaulttheme/includes/scripts.js
@@ -1,17 +1,17 @@
 
 /**
-  * Toggle checked flag of checkboxes with name fieldname, inside same form as clicked-element.
+  * Toggle checked flag of checkboxes with name field name, inside same form as clicked-element.
   *
   */
 	function toggleChecked(clicked, fieldname) {
-		for (i = 0; i < clicked.form.elements[fieldname].length; i++) {
+		for (var i = 0; i < clicked.form.elements[fieldname].length; i++) {
 			clicked.form.elements[fieldname][i].checked = clicked.checked;
 		}
 	}
 	
 	function toggleProjectPermissionsChecked(clicked) {
 		form = clicked.form;
-		for (i = 0; i < form.elements.length; i++) {
+		for (var i = 0; i < form.elements.length; i++) {
 			if (form.elements[i].name.indexOf(clicked.name) > -1) {
 				form.elements[i].checked = clicked.checked;
 			}
