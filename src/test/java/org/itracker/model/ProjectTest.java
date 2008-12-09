e74e1bc605b545b7b04be6f282ae615e7e2c9dbf
ranks <ranks@users.sf.net>
2008-12-09 13:17:26 +0000

    adding comparator tests

--- src/test/java/org/itracker/model/ProjectTest.java
+++ src/test/java/org/itracker/model/ProjectTest.java
@@ -1,8 +1,10 @@
 package org.itracker.model;
+import static org.itracker.Assert.*;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import org.itracker.services.util.UserUtilities;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -35,6 +37,26 @@ public class ProjectTest {
 		assertNotNull("toString", pro.toString());
 	}
 	
+	@Test
+	public void testProjectComparator() {
+		Project entityA = new Project("a");
+		Project entityB = new Project("b");
+
+		assertEntityComparator("project comparator", Project.PROJECT_COMPARATOR, entityA, entityB);
+		assertEntityComparator("project comparator", Project.PROJECT_COMPARATOR, entityA, null);
+		assertEntityComparatorEquals("project comparator", Project.PROJECT_COMPARATOR, entityA, entityA);
+		
+		entityA.setName(entityB.getName());
+		entityA.setId(1);
+		entityB.setId(2);
+
+		assertEntityComparator("project comparator", Project.PROJECT_COMPARATOR, entityA, entityB);
+		assertEntityComparator("project comparator", Project.PROJECT_COMPARATOR, entityA, null);
+		
+		entityB.setId(entityA.getId());
+		assertEntityComparatorEquals("project comparator", Project.PROJECT_COMPARATOR, entityA, entityB);
+		assertEntityComparatorEquals("project comparator", Project.PROJECT_COMPARATOR, entityA, entityA);
+	}
 	
 	@Before
     public void setUp() throws Exception {
