7bef99c6ba7ac21e2554a179be34b7072f760c30
ranks <ranks@users.sf.net>
2008-12-09 13:35:05 +0000

    adding comparator tests

--- src/test/java/org/itracker/model/ReportTest.java
+++ src/test/java/org/itracker/model/ReportTest.java
@@ -1,4 +1,6 @@
 package org.itracker.model;
+import static org.itracker.Assert.assertEntityComparatorEquals;
+import static org.itracker.Assert.assertEntityComparator;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.After;
@@ -13,6 +15,38 @@ public class ReportTest {
 		assertNotNull("toString", rpt.toString());
 	}
 	
+	@Test
+	public void testNameComparator() {
+		Report entityA = new Report();
+		Report entityB = new Report();
+		entityA.setName("a");
+		entityB.setName("b");
+
+		assertEntityComparator("name comparator", Report.NAME_COMPARATOR, entityA, entityB);
+		assertEntityComparator("name comparator", Report.NAME_COMPARATOR, entityA, null);
+		assertEntityComparatorEquals("name comparator", Report.NAME_COMPARATOR, entityA, entityA);
+
+		entityA.setName(entityB.getName());
+		entityA.setNameKey("a");
+		entityB.setNameKey("b");
+
+		assertEntityComparator("name comparator", Report.NAME_COMPARATOR, entityA, entityB);
+		assertEntityComparator("name comparator", Report.NAME_COMPARATOR, entityA, null);
+		assertEntityComparatorEquals("name comparator", Report.NAME_COMPARATOR, entityA, entityA);
+		
+		entityA.setNameKey(entityB.getNameKey());
+		entityA.setId(1);
+		entityB.setId(2);
+
+		assertEntityComparator("name comparator", Report.NAME_COMPARATOR, entityA, entityB);
+		assertEntityComparator("name comparator", Report.NAME_COMPARATOR, entityA, null);
+		
+		entityA.setId(entityB.getId());
+		
+		assertEntityComparatorEquals("name comparator", Report.NAME_COMPARATOR, entityA, entityB);
+		assertEntityComparatorEquals("name comparator", Report.NAME_COMPARATOR, entityA, entityA);
+		
+	}
 	
 	@Before
     public void setUp() throws Exception {
