729ab06a6f8975ed8c16a0986ac0ec86c132e4c5
ranks <ranks@users.sf.net>
2009-03-14 16:47:25 +0000

    failed test fixed, history is required when issue is created

--- src/test/java/org/itracker/selenium/CreateIssueTest.java
+++ src/test/java/org/itracker/selenium/CreateIssueTest.java
@@ -1,12 +1,15 @@
 package org.itracker.selenium;
 
-import com.dumbster.smtp.SimpleSmtpServer;
-import com.dumbster.smtp.SmtpMessage;
 import java.io.IOException;
 import java.util.Iterator;
+
+import org.apache.log4j.Logger;
 import org.itracker.model.User;
 import org.itracker.persistence.dao.UserDAO;
 import org.junit.Test;
+
+import com.dumbster.smtp.SimpleSmtpServer;
+import com.dumbster.smtp.SmtpMessage;
 
 /**
  * Verifies the functionality of new issue creation.
@@ -14,7 +17,10 @@ import org.junit.Test;
  * @author Andrey Sergievskiy <seas@andreysergievskiy.com>
  */
 public class CreateIssueTest extends AbstractSeleniumTestCase {
-    /**
+	
+	private static final Logger log = Logger.getLogger(CreateIssueTest.class);
+
+	/**
      * 1. Login into the system with some particular user
      *    (admin_test1 in our case, which already has 4 issues, 2 of them
      *     are unassigned).
@@ -31,6 +37,10 @@ public class CreateIssueTest extends AbstractSeleniumTestCase {
     @Test
     public void testCreateUnassignedIssue() throws IOException {
         closeSession();
+
+        final String descriptionValue = "Issue to be assigned.";
+        final String historyValue = "Issue to be assigned history.";
+        
         selenium.open("http://" + applicationHost + ":" + applicationPort + "/"
                 + applicationPath);
         assertTrue(selenium.isElementPresent("//.[@name='login']"));
@@ -56,7 +66,9 @@ public class CreateIssueTest extends AbstractSeleniumTestCase {
         assertTrue(selenium.isElementPresent("severity"));
         assertTrue(selenium.isElementPresent("versions"));
         
-        selenium.type("//td[@id='description']/input", "Issue to be unassigned.");
+        selenium.type("//td[@id='description']/input", descriptionValue);
+		selenium.type("history", historyValue);
+		
         selenium.select("//td[@id='ownerId']/select", "value=-1");
         final UserDAO userDao = (UserDAO)applicationContext.getBean("userDAO");
         final User user = userDao.findByLogin("admin_test1");
@@ -65,29 +77,38 @@ public class CreateIssueTest extends AbstractSeleniumTestCase {
         selenium.select("//td[@id='creatorId']/select", "value=" + userId);
         
         final SimpleSmtpServer smtpServer = SimpleSmtpServer.start(2525);
-        selenium.click("//td[@id='submit']/input");
-        selenium.waitForPageToLoad(SE_TIMEOUT);
-        assertEquals(smtpServer.getReceivedEmailSize(), 1);
-        final Iterator<SmtpMessage> iter =
-                (Iterator<SmtpMessage>)smtpServer.getReceivedEmail();
-        // Checking email notification for creator.
-        final SmtpMessage smtpMessage1 = iter.next();
-        final String smtpMessageBody1 = smtpMessage1.getBody();
-        assertTrue(smtpMessageBody1.contains("Issue to be unassigned."));
-        smtpServer.stop();
+        
+        try {
+	        selenium.click("//td[@id='submit']/input");
+	        selenium.waitForPageToLoad(SE_TIMEOUT);
+	        assertEquals("sent notifications", 1, smtpServer.getReceivedEmailSize());
+	        final Iterator<SmtpMessage> iter =
+	                (Iterator<SmtpMessage>)smtpServer.getReceivedEmail();
+	        // Checking email notification for creator.
+	        final SmtpMessage smtpMessage1 = iter.next();
+	        log.info("testCreateUnassignedIssue, raw:\n" + smtpMessage1.getBody());
+	        final String smtpMessageBody1 = smtpMessage1.getBody();
+	        log.info("testCreateUnassignedIssue, received:\n" + smtpMessageBody1);
+	        assertTrue(smtpMessageBody1.contains(descriptionValue));
+	        assertTrue(smtpMessageBody1.contains(historyValue));
+        } finally {
+        	smtpServer.stop();
+        }
+        
+        
         
         // Check that the total number of issues is 5 now (4 from db + 1 our).
         assertTrue(selenium.isElementPresent("issues"));
         assertEquals(5, selenium.getXpathCount("//tr[starts-with(@id, 'issue.')]"));
-        assertTrue(selenium.isElementPresent("//tr[starts-with(@id, 'issue.')]/td[11][text()='Issue to be unassigned.']"));
+        assertTrue(selenium.isElementPresent("//tr[starts-with(@id, 'issue.')]/td[11][text()='" + descriptionValue + "']"));
         
         selenium.open("http://" + applicationHost + ":" + applicationPort + "/"
                 + applicationPath + "/portalhome.do");
         // Check that just created issue has appeared in "Unassigned" area.
-        assertTrue(selenium.isElementPresent("//tr[starts-with(@id,'unassignedIssue.')]/td[5][text()='test_name']/../td[11][text()='Issue to be unassigned.']"));
+        assertTrue(selenium.isElementPresent("//tr[starts-with(@id,'unassignedIssue.')]/td[5][text()='test_name']/../td[11][text()='" + descriptionValue + "']"));
         
         // Check that just created issue has appeared in "Created" area.
-        assertTrue(selenium.isElementPresent("xpath=//tr[starts-with(@id,'createdIssue.')]/td[5][text()='test_name']/../td[11][text()='Issue to be unassigned.']"));
+        assertTrue(selenium.isElementPresent("xpath=//tr[starts-with(@id,'createdIssue.')]/td[5][text()='test_name']/../td[11][text()='" + descriptionValue + "']"));
         
         // Check that number of watched items is 0.
         assertEquals(0, selenium.getXpathCount("//tr[starts-with(@id, 'watchedIssue.')]"));        
@@ -100,6 +121,10 @@ public class CreateIssueTest extends AbstractSeleniumTestCase {
     @Test
     public void testCreateAssignedIssue() throws IOException {
         closeSession();
+        
+        final String descriptionValue = "Issue to be unassigned.";
+        final String historyValue = "Issue to be unassigned history.";
+        
         selenium.open("http://" + applicationHost + ":" + applicationPort + "/"
                 + applicationPath);
         assertTrue(selenium.isElementPresent("//.[@name='login']"));
@@ -124,43 +149,50 @@ public class CreateIssueTest extends AbstractSeleniumTestCase {
         assertTrue(selenium.isElementPresent("creatorId"));
         assertTrue(selenium.isElementPresent("severity"));
         assertTrue(selenium.isElementPresent("versions"));
+
         
-        selenium.type("//td[@id='description']/input", "Issue to be assigned.");
+		selenium.type("//input[@name='description']", descriptionValue);
+		selenium.type("history", historyValue);
         final UserDAO userDao = (UserDAO)applicationContext.getBean("userDAO");
         final User user = userDao.findByLogin("admin_test1");
         assertTrue(null != user);
         final long userId = user.getId();
         selenium.select("//td[@id='ownerId']/select", "value=" + userId);        
         selenium.select("//td[@id='creatorId']/select", "value=" + userId);
-        
         final SimpleSmtpServer smtpServer = SimpleSmtpServer.start(2525);
-        selenium.click("//td[@id='submit']/input");
-        selenium.waitForPageToLoad(SE_TIMEOUT);
-        assertEquals(smtpServer.getReceivedEmailSize(), 2);
-        final Iterator<SmtpMessage> iter =
-                (Iterator<SmtpMessage>)smtpServer.getReceivedEmail();
-        // Checking email notification for creator.
-        final SmtpMessage smtpMessage1 = iter.next();
-        final String smtpMessageBody1 = smtpMessage1.getBody();
-        assertTrue(smtpMessageBody1.contains("Issue to be assigned."));
-        // Checking email notification for owner.
-        final SmtpMessage smtpMessage2 = iter.next();
-        final String smtpMessageBody2 = smtpMessage2.getBody();
-        assertTrue(smtpMessageBody2.contains("Issue to be assigned."));
-        smtpServer.stop();
-        
+        try {
+	        selenium.click("//td[@id='submit']/input");
+	        selenium.waitForPageToLoad(SE_TIMEOUT);
+	        assertEquals("sent notifications", 2, smtpServer.getReceivedEmailSize());
+	        final Iterator<SmtpMessage> iter =
+	                (Iterator<SmtpMessage>)smtpServer.getReceivedEmail();
+	        // Checking email notification for creator.
+	        final SmtpMessage smtpMessage1 = iter.next();
+	        log.info("testCreateAssignedIssue, raw:\n " + smtpMessage1.getBody());
+	        final String smtpMessageBody1 = smtpMessage1.getBody();
+	        log.info("testCreateAssignedIssue, received:\n " + smtpMessageBody1);
+	        assertTrue(smtpMessageBody1.contains(descriptionValue));
+	        assertTrue(smtpMessageBody1.contains(historyValue));
+	        // Checking email notification for owner.
+	        final SmtpMessage smtpMessage2 = iter.next();
+	        final String smtpMessageBody2 = smtpMessage2.getBody();
+	        assertTrue(smtpMessageBody2.contains(descriptionValue));
+	        assertTrue(smtpMessageBody2.contains(historyValue));
+		} finally {
+        	smtpServer.stop();
+        }
         // Checking that our new issue has appeared in "View Issues".
         assertTrue(selenium.isElementPresent("issues"));
         assertEquals(5, selenium.getXpathCount("//tr[starts-with(@id, 'issue.')]"));
-        assertTrue(selenium.isElementPresent("//tr[starts-with(@id, 'issue.')]/td[11][text()='Issue to be assigned.']"));
+        assertTrue(selenium.isElementPresent("//tr[starts-with(@id, 'issue.')]/td[11][text()='" + descriptionValue + "']"));
         
         selenium.open("http://" + applicationHost + ":" + applicationPort + "/"
                 + applicationPath + "/portalhome.do");
         // Checking that our new issue has not appeared in "Unassigned" area.
-        assertFalse(selenium.isElementPresent("//tr[starts-with(@id,'unassignedIssue.')]/td[5][text()='test_name']/../td[11][text()='Issue to be assigned.']"));
+        assertFalse(selenium.isElementPresent("//tr[starts-with(@id,'unassignedIssue.')]/td[5][text()='test_name']/../td[11][text()='" + descriptionValue + "']"));
         
         // Checking that our new issue has appeared in "Created" area.
-        assertTrue(selenium.isElementPresent("//tr[starts-with(@id,'createdIssue.')]/td[5][text()='test_name']/../td[11][text()='Issue to be assigned.']"));
+        assertTrue(selenium.isElementPresent("//tr[starts-with(@id,'createdIssue.')]/td[5][text()='test_name']/../td[11][text()='" + descriptionValue + "']"));
         
         // Check that "Watched" area is still empty.
         assertEquals(0, selenium.getXpathCount("//tr[starts-with(@id, 'watchedIssue.')]"));
@@ -182,6 +214,8 @@ public class CreateIssueTest extends AbstractSeleniumTestCase {
                 "dataset/issuehistorybean_dataset.xml"
         };
     }
+
+   
 
     @Override
     protected String[] getConfigLocations() {
